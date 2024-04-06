5f4bd7a109e18533278c2d3d428864fd83f7f23b
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2024-04-06 21:32:08 +0530

    supply borrow

---
+++ src/pages/Market.js
@@ -0,0 +1,15 @@
+import BorrowModal from '@/components/BorrowModal'
+import SupplyModal from '@/components/SupplyModal'
+import React from 'react'
+
+function Market() {
+  return (
+    <div>Market
+
+    <BorrowModal/>
+    <SupplyModal/>
+    </div>
+  )
+}
+
+export default Market
